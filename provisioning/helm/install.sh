d4e510239db4fd4113daa8de2abcd1e1c0bb866d
Subhakar Kotta <subhakarkotta@gmail.com>
2019-02-24 19:36:23 +0530

    Update install.sh

--- provisioning/helm/install.sh
+++ provisioning/helm/install.sh
@@ -7,6 +7,6 @@ chmod +x get_helm.sh
 
 
 figlet TILLER
-su - vagrant -c "kubectl --namespace kube-system create serviceaccount tiller"
-su - vagrant -c "kubectl create clusterrolebinding tiller --clusterrole cluster-admin --serviceaccount=kube-system:tiller"
-su - vagrant -c "helm init --service-account tiller --upgrade"
+kubectl --namespace kube-system create serviceaccount tiller
+kubectl create clusterrolebinding tiller --clusterrole cluster-admin --serviceaccount=kube-system:tiller
+helm init --service-account tiller --upgrade
