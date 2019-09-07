a0f988ac3c8be1089e68a005a7c342c14f73f105
louispauls <49291885+louispauls@users.noreply.github.com>
2019-09-07 12:33:08 -0400

    Update install.sh
    https://github.com/kubernetes/kubeadm/issues/1575

--- provisioning/vm-worker/install.sh
+++ provisioning/vm-worker/install.sh
@@ -5,7 +5,7 @@ figlet WORKER
 echo "[TASK 1] Start docker/kubelet"
 systemctl start docker && systemctl enable docker
 systemctl start kubelet && systemctl enable kubelet
-sed -i 's/cgroup-driver=systemd/cgroup-driver=cgroupfs/g' /etc/systemd/system/kubelet.service.d/10-kubeadm.conf
+sed -i 's/cgroup-driver=systemd/cgroup-driver=cgroupfs/g' /usr/lib/systemd/system/kubelet.service.d/10-kubeadm.conf
 systemctl daemon-reload
 systemctl restart kubelet
 
